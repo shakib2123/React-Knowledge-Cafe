2c39a781a581fa1807d0495a7d08a1c12d713b8a
shakib2123 <mshakibalhasan21@gmail.com>
2023-09-11 22:48:34 +0600

    header section created

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("blog.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+  console.log(blogs);
+  return <div></div>;
+};
+
+export default Blogs;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import profile from "../../assets/profile.png";
+const Header = () => {
+  return (
+    <div className="bg-gradient-to-r from-pink-500 to-yellow-500 ">
+      <header className="container mx-auto flex justify-between py-2">
+        <h1 className="text-4xl font-bold text-white">Knowledge Cafe</h1>
+        <img src={profile} alt="" />
+      </header>
+    </div>
+  );
+};
+
+export default Header;
